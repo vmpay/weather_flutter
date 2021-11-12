fec4df17f4f3950bf9eab812a9b724daca1e1ce6
Andrew <vmpay.eu@gmail.com>
2021-11-12 13:18:00 +0100

    Delayed splash screen transition

--- lib/features/splash_screen.dart
+++ lib/features/splash_screen.dart
@@ -6,32 +6,32 @@ class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: GestureDetector(
-          onTap: () {
-            Modular.to.navigate('/weather');
-          },
-          child: Column(
-            children: [
-              Image.asset('assets/graphics/group_2.png'),
-              const Padding(
-                padding: EdgeInsets.only(top: 90.0),
-                child: Text(
-                  'Welcome to',
-                  style: TextStyle(fontSize: 32),
-                ),
-              ),
-              RichText(
-                text: const TextSpan(
-                  text: 'WEATHER APP',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 32,
-                      color: Colors.black),
-                ),
-              ),
-            ],
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 2000), () {
+      Modular.to.navigate('/weather');
+    });
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset('assets/graphics/group_2.png'),
+          const Padding(
+            padding: EdgeInsets.only(top: 90.0),
+            child: Text(
+              'Welcome to',
+              style: TextStyle(fontSize: 32),
+            ),
           ),
-        ),
-      );
+          RichText(
+            text: const TextSpan(
+              text: 'WEATHER APP',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                  color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
